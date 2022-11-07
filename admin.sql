eb2c848cf764f84cb292b0642eaf6fcba575605e
Andrew <andrew.koman.02@gmail.com>
2022-11-06 19:33:24 -0500

    Fixed

--- admin.sql
+++ admin.sql
@@ -7,33 +7,32 @@ DELIMITER //
 SET @master_password = 'CS3425';
 
 -- Create Instructors account with temp password
-CREATE PROCEDURE create_instructor(IN ins_username VARCHAR(20), IN ins_name VARCHAR(50), OUT temp_password CHAR(10))
+CREATE PROCEDURE create_instructor(IN ins_username VARCHAR(20), IN ins_name VARCHAR(50))
 BEGIN
-    INSERT INTO TABLE instructors VALUES(NULL, ins_username, ins_name, SHA2(@master_password, 256));
+    INSERT INTO Instructors VALUES(NULL, ins_username, ins_name, SHA2(@master_password, 256));
     RETURN @master_password;
 END//
 
 -- Create Instructors account with temp password
-CREATE PROCEDURE create_student(IN stu_username VARCHAR(20), IN stu_name VARCHAR(50), OUT temp_password CHAR(10))
+CREATE PROCEDURE create_student(IN stu_username VARCHAR(20), IN stu_name VARCHAR(50))
 BEGIN
-    INSERT INTO TABLE students VALUES(NULL, stu_username, stu_name, SHA2(@master_password, 256));
+    INSERT INTO Students VALUES(NULL, stu_username, stu_name, SHA2(@master_password, 256));
     RETURN @master_password;
 END//
 
 -- Create course
-CREATE PROCEDURE create_course(IN title VARCHAR(50), IN credits numeric(2,1))
+CREATE PROCEDURE create_course(IN cTitle VARCHAR(50), IN cCredits numeric(2,1))
 BEGIN
     -- instructor_id is by default NULL
-    INSERT INTO TABLE courses VALUES(NULL, NULL, title, credits);
+    INSERT INTO Courses VALUES(NULL, NULL, cTitle, cCredits);
 END//
 
 -- Assign instructor to create course
 CREATE PROCEDURE assign_teacher(IN ins_id INT, IN crse_id INT)
-    UPDATE courses 
+BEGIN
+    UPDATE Courses
     SET instructor_id = ins_id
     WHERE course_id = crse_id;
-BEGIN
-
 END//
 
 DELIMITER ;
