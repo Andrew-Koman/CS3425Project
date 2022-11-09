f13f3cfb7232ae232769d8e82da2bfcbe7e10b7c
Andrew <andrew.koman.02@gmail.com>
2022-11-08 19:53:29 -0500

    Think it works now lol

--- autograder.sql
+++ autograder.sql
@@ -1,14 +1,13 @@
 DROP TRIGGER IF EXISTS autograder;
 DELIMITER //
 CREATE TRIGGER autograder
-    AFTER UPDATE ON takes_exam
+    AFTER UPDATE ON student_answers
     FOR EACH ROW
-    IF OLD.start_time IS NOT NULL AND OLD.end_time IS NOT NULL THEN
-        UPDATE takes_exam
-        SET score = ( SELECT sum(points * is_correct)
-        FROM student_answers NATURAL JOIN questions NATURAL JOIN answers
-        WHERE student_id = OLD.student_id
-        GROUP BY exam_id, student_id ) WHERE student_id = OLD.student_id;
-    END IF;
-//
+    BEGIN
+    UPDATE takes_exam
+    SET score = ( SELECT sum(points * is_correct)
+    FROM student_answers NATURAL JOIN questions NATURAL JOIN answers
+    WHERE student_id = NEW.student_id )
+    WHERE student_id = NEW.student_id;
+    END//
 DELIMITER ;
