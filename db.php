884b908986d5c99f440e140f3243fa5bd8ade50e
dmdimitr <dmdimitr@mtu.edu>
2022-12-09 12:09:26 -0500

    moved non-DB related functions to their own file

--- db.php
+++ db.php
@@ -7,15 +7,6 @@ function connectDB(): PDO
     return $dbh;
 }
 
-function is_logged_on(){
-    // Checks if the username is set, then returns to login if it isn't
-    session_start();
-    if(!isset($_SESSION['username'])){
-        header("Location: login.php");
-        return;
-    }
-}
-
 /**
  * @param $user
  * @param $password
@@ -152,29 +143,4 @@ function getCoursesIns(string $username) {
     createTable($result, $headers);
 }
 
-function createTable( array $result, array $headers) {
-    if (count($result[0])/2 != count($headers)){
-        return;
-    }
-
-    echo "<pre>";
-    print_r($result);
-    echo "</pre>";
-
-    $numExams = count($result);
-    echo "<table>";
-    echo "<tr>";
-    foreach ($headers as $header) {
-        echo "<th>$header</th>";
-    }
-    echo "</tr>";
-    for ($i = 0; $i < $numExams; $i++){
-        echo "<tr>";
-        $row = $result[$i];
-        for ($j = 0; $j < count($row) / 2; $j++){
-            echo "<td>$row[$j]</td>";
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
-}
+?>
---
+++ web_funcs.php
@@ -0,0 +1,38 @@
+<?php
+function is_logged_on(){
+    // Checks if the username is set, then returns to login if it isn't
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header("Location: login.php");
+        return;
+    }
+}
+
+function createTable( array $result, array $headers) {
+    if (count($result[0])/2 != count($headers)){
+        return;
+    }
+
+    echo "<pre>";
+    print_r($result);
+    echo "</pre>";
+
+    $numExams = count($result);
+    echo "<table>";
+    echo "<tr>";
+    foreach ($headers as $header) {
+        echo "<th>$header</th>";
+    }
+    echo "</tr>";
+    for ($i = 0; $i < $numExams; $i++){
+        echo "<tr>";
+        $row = $result[$i];
+        for ($j = 0; $j < count($row) / 2; $j++){
+            echo "<td>$row[$j]</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+?>
