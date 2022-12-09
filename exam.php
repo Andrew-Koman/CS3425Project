19765b5099d23aacfa477a5f18b3df2f613147dc
dmdimitr <dmdimitr@mtu.edu>
2022-12-09 15:28:46 -0500

    All exam-related functions for both students and instructors

---
+++ exam.php
@@ -0,0 +1,36 @@
+<!-- 
+    All exam-related functions for both students and instructors
+ -->
+<?php
+include "db.php";
+
+function getQuestions(int $exam_id):array{
+    return [];
+}
+
+function getChoices(int $question_id):array{
+    return [];
+}
+
+// Submits single answer
+function submitAnswer(int $student_id, int $question_id){
+    
+}
+
+// Marks exam as started
+function startExam(int $student_id, int $exam_id){
+    
+}
+
+// Marks exam as completed
+function completeExam(int $student_id, int $exam_id){
+    
+}
+
+// Gets all info from the takes_exam table
+function getStudentExams(int $exam_id){
+
+}
+
+
+?>
