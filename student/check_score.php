51337a4e187e474fdfaefe03527fe112fea32d93
dmdimitr <dmdimitr@mtu.edu>
2022-12-11 15:09:33 -0500

    initial

---
+++ student/check_score.php
@@ -0,0 +1,38 @@
+<?php
+include "db.php";
+
+session_start();
+is_logged_on();
+
+if (!isset($_POST['check_score']) || !isset($_POST['exam'])){
+    header("Location: main.php");
+    return;
+}
+
+$course_id = $_POST['course'];
+$exam_name = $_POST['exam'];
+$student = $_SESSION['student']; 
+$student_id = getStudentId($student);
+
+$dbh = connectDB();
+$statement = $dbh -> prepare('
+    SELECT score, start_time, end_time, (end_time - start_time)
+    FROM takes_exam
+    NATURAL JOIN exams
+    WHERE course_id = :course_id
+    AND exams.name = :exam_name
+    AND student_id = :student_id');
+
+$statement -> bindParam(":course_id", $course_id);
+$statement -> bindParam(":exam_name", $exam_name);
+$statement -> bindParam(":student_id", $student_id);
+$statement -> execute();
+$val = $statement -> fetch();
+$head = array("score", "start_time", "end_time", "time_(sec)");
+createTable($val, $head)
+?>
+
+
+<form method="post" action="main.php">
+        <input type="submit" value="Go Back" name="go_main">
+</form>
