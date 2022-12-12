409bb0069ec1901adddb44f31b90aa9878ab211e
dmdimitr <dmdimitr@mtu.edu>
2022-12-11 22:24:33 -0500

    added tables

--- student/main.php
+++ student/main.php
@@ -1,3 +1,4 @@
+<style><?php include '../style.css'; ?></style>
 <?php
     session_start();
     include "../db.php";
@@ -9,8 +10,14 @@
         return;
     }
 
+    // echo "<pre>";
+    // print_r($_POST);
+    // print_r($_SESSION);
+    // echo "</pre>";
+
     $username = $_SESSION['username'];
     $name = getName($username);
+    $student_id = getStudentId(($username));
     echo "<p>Welcome $name!</p>"; 
 ?>
 
@@ -19,21 +26,52 @@
 </form>
 
 <h1>Enrolled Courses</h1>
-
 <?php
-echo "!! table !!";
+$header = array("course_id", "title", "credits", "instructor");
+$dbh = connectDB();
+$statement = $dbh -> prepare("SELECT course_id, title, credits, instructors.name
+    FROM courses
+    NATURAL JOIN instructors
+    WHERE course_id IN (
+        SELECT course_id
+        FROM takes_course
+        WHERE student_id = :student_id
+        )");
+$statement -> bindParam(":student_id", $student_id);
+$statement -> execute();
+$vals = $statement -> fetchAll();
+createTable($vals, $header);
 ?>
 
 <h1>Exams</h1>
-
 <?php
-echo "!! table !!";
+$header = array("course_id", "exam_name", "open_time", "close_time", "total_points", "start_time", "end_time");
+$statement = $dbh -> prepare("SELECT course_id, name, open_time, close_time, score, start_time, end_time
+    FROM exams
+    NATURAL JOIN takes_course
+    NATURAL LEFT JOIN takes_exam
+    WHERE student_id = :student_id");
+$statement -> bindParam(":student_id", $student_id);
+$statement -> execute();
+$vals = $statement -> fetchAll();
+createTable($vals, $header);
 ?>
 
 <h1>Available Courses</h1>
-
 <?php
-echo "!! table !!";
+$header = array("course_id", "title", "credits", "instructor");
+$statement = $dbh -> prepare("SELECT course_id, title, credits, instructors.name
+    FROM courses
+    NATURAL JOIN instructors
+    WHERE course_id NOT IN (
+        SELECT course_id
+        FROM takes_course
+        WHERE student_id = :student_id
+        )");
+$statement -> bindParam(":student_id", $student_id);
+$statement -> execute();
+$vals = $statement -> fetchAll();
+createTable($vals, $header);
 ?>
 
 <p>
@@ -52,5 +90,5 @@ echo "!! table !!";
     <br>
     <button type="submit" formaction="new_course.php" value="new_course" name="new_course">New Course</button>
     <button type="submit" formaction="take_exam.php" value="take_exam" name="take_exam">Take Exam</button>
-    <button type="submit" formaction="check_score.php" value="check_score" name="take_exam">Check Score</button>
+    <button type="submit" formaction="check_score.php" value="check_score" name="check_score">Check Score</button>
 </form>
