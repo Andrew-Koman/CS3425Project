95dd7071169f54ac02d761d8f46176697da514d8
dmdimitr <dmdimitr@mtu.edu>
2022-12-11 22:38:26 -0500

    take exam boilerplate

--- student/take_exam.php
+++ student/take_exam.php
@@ -1,4 +1,23 @@
 <?php
-require "../exam.php";
+session_start();
+include "../exam.php";
 
+if( !isset($_POST["take_exam"]) ) {
+    header("Location: main.php");
+    die();
+}
+
+echo "<pre>";
+echo "<p>Post:</p>";
+print_r($_POST);
+echo "<p>Session:</p>";
+print_r($_SESSION);
+echo "</pre>";
+
+if (!examExists($_POST["exam"], $_POST["course"])){
+    echo "<p style='color: red'>Error. Could not find exam</p>";
+} else {
+    $exam_id = getExamId($_POST["exam"]);
+    $questions = getQuestions($exam_id);
+}
 ?>
