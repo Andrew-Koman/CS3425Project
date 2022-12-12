5553b8ec84ec2f9a3c154872276d18d6a4f0c3cd
Andrew <andrew.koman.02@gmail.com>
2022-12-12 02:03:16 -0500

    Takes exam works, just needs to check to see if exam is open or not

--- exam.php
+++ exam.php
@@ -30,19 +30,87 @@ function getChoices(int $question_id):array{
     return $statement -> fetchAll();
 }
 
-// Submits single answer
-function submitAnswer(int $student_id, int $question_id){
-    
-}
 
 // Marks exam as started
 function startExam(int $student_id, int $exam_id){
-    
+    $dbh = connectDB();
+    $statement = $dbh->prepare("INSERT INTO takes_exam (student_id, exam_id, start_time) VALUES (:student, :exam, NOW())");
+    $statement->bindParam(":student", $student_id);
+    $statement->bindParam(":exam", $exam_id);
+    $statement->execute();
 }
 
-// Marks exam as completed
+// Marks exam as completed and sends answers to database
 function completeExam(int $student_id, int $exam_id){
-    
+    $dbh = connectDB();
+    $questions = getQuestions($exam_id);
+    try {
+        $dbh->beginTransaction();
+        $statement = $dbh->prepare("UPDATE takes_exam SET end_time = NOW() WHERE student_id = :student AND exam_id = :exam");
+        $statement->bindParam(":student", $student_id);
+        $statement->bindParam(":exam", $exam_id);
+        $statement->execute();
+
+        foreach ($questions as $question) {
+            $answer_id = getAnswerId($_POST[$question["question_id"]], $question["question_id"]);
+
+            $statement = $dbh->prepare("INSERT INTO student_answers (student_id, answer_id) VALUES (:student, :answer)");
+            $statement->bindParam(":student", $student_id);
+            $statement->bindParam(":answer", $answer_id);
+            $statement->execute();
+        }
+        $dbh->commit();
+    }
+    catch (PDOException $exception) {
+        $dbh -> rollBack();
+        echo "<p style='color: red'>ERROR. Exam could not be submitted</p>";
+        echo "<pre style='color: red'>";
+        print_r($exception);
+        echo "</pre>";
+        echo "<form action='student/main.php'><button type='submit'>Go Back</button></form>";
+        unset($_SESSION["exam"]);
+        unset($_SESSION["course"]);
+        die();
+    }
+}
+
+function getAnswerId(string $answer_letter, int $question_id) : int{
+    $dbh = connectDB();
+    $statement = $dbh->prepare("SELECT answer_id FROM answers WHERE question_id = :question AND answer_letter = :answer");
+    $statement->bindParam(":question", $question_id);
+    $statement->bindParam(":answer", $answer_letter);
+    $statement->execute();
+    return ($statement->fetch())[0];
+}
+
+// prints the results tables
+function printResults(){
+    $dbh = connectDB();
+    $student = getStudentId($_SESSION["username"]);
+    $exam = getExamId($_SESSION["exam"], $_SESSION["course"]);
+    $dbh -> beginTransaction();
+    $statement = $dbh -> prepare("SELECT score, start_time, end_time, TIMESTAMPDIFF(SECOND, start_time, end_time) durration_in_sec
+                            FROM takes_exam WHERE exam_id = :exam AND student_id = :student");
+    $statement->bindParam(":student", $student);
+    $statement->bindParam(":exam", $exam);
+    $statement->execute();
+    $stats_results = $statement->fetchAll();
+
+    $statement = $dbh -> prepare("SELECT a.question_id, description, a.answer_letter, s.earned_points,
+                                (SELECT ans.answer_letter FROM answers ans WHERE ans.is_correct = TRUE AND ans.question_id = q.question_id) correct
+                                FROM student_answers s JOIN answers a on s.answer_id = a.answer_id
+                                JOIN questions q on a.question_id = q.question_id
+                                WHERE student_id = :student");
+    $statement->bindParam(":student", $student);
+    $statement->execute();
+    $question_results = $statement->fetchAll();
+
+    $dbh->commit();
+    $headers = array("score", "start_time", "end_time", "durraction_in_sec");
+    createTable($stats_results, $headers);
+    echo "<br>";
+    $headers = array("q_id", "description", "yourAnswer", "yourPoints", "CorrectAnswer");
+    createTable($question_results, $headers);
 }
 
 
--- student/take_exam.php
+++ student/take_exam.php
@@ -9,9 +9,14 @@
 session_start();
 include "../exam.php";
 
-if( !isset($_POST["take_exam"]) ) {
+if( !isset($_POST["take_exam"]) && !isset($_POST["submit"])) {
     header("Location: main.php");
     die();
+}
+
+if( isset($_POST["exam"]) && isset($_POST["course"])) {
+    $_SESSION["exam"] = $_POST["exam"];
+    $_SESSION["course"] = $_POST["course"];
 }
 
 echo "<pre>";
@@ -21,10 +26,36 @@ echo "<p>Session:</p>";
 print_r($_SESSION);
 echo "</pre>";
 
-if (!examExists($_POST["exam"], $_POST["course"])){
+if (!examExists($_SESSION["exam"], $_SESSION["course"])){
     echo "<p style='color: red'>Error. Could not find exam</p>";
-} else {
-    $exam_id = getExamId($_POST["exam"], $_POST["course"]);
+} else if (!isset($_POST["submit"])){
+    $exam_id = getExamId($_SESSION["exam"], $_SESSION["course"]);
     $questions = getQuestions($exam_id);
+    startExam(getStudentId($_SESSION["username"]), getExamId($_SESSION["exam"], $_SESSION["course"]));
+    echo "<form method='post' action='take_exam.php'>";
+
+    $index = 0;
+    foreach ($questions as $question) {
+        echo "<p>Q" . $question["question_id"] . ": " . $question["description"] . " (" . $question["points"] . " points)</p>";
+
+        $choices = getChoices($question["question_id"]);
+        echo "<div style='margin-left: 20px'>";
+        foreach ($choices as $choice) {
+            echo "<input type='radio' id='" . $question["question_id"] . "' name='" . $question["question_id"] . "' value = '" . $choice["answer_letter"] . "'>";
+            echo "<label for='" . $question["question_id"] . "'> " . $choice["answer_letter"] . ": " . $choice["answer"] . "<br>";
+        }
+        echo "</div>";
+        $index++;
+    }
+    echo "<input type='submit' name='submit'>";
+    echo "</form>";
+} else {
+    completeExam(getStudentId($_SESSION["username"]), getExamId($_SESSION["exam"], $_SESSION["course"]));
+    printResults();
+    unset($_SESSION["exam"]);
+    unset($_SESSION["course"]);
+    echo "<form action='main.php'><button type='submit'>Go Back</button></form>";
 }
+
+
 ?>
