a5fa7e280988caa4c9ab0ba5ec4031a3f722c124
Andrew <andrew.koman.02@gmail.com>
2022-12-13 14:35:21 -0500

    Moved stuff

--- instructor/check_score.php
+++ instructor/check_score.php
@@ -22,52 +22,6 @@ else {
     echo "<br>";
     getSubmissions();
 }
-
-function getExamInfo() {
-    $dbh = connectDB();
-    $dbh -> beginTransaction();
-    $statement = $dbh -> prepare("SELECT 
-        course_id, 
-        (SELECT COUNT(student_id) FROM takes_course), 
-        exams.name, 
-        COUNT(student_id), 
-        MIN(score), 
-        MAX(score),
-        AVG(score)
-    FROM exams
-    NATURAL JOIN takes_exam
-    WHERE course_id = :course_id
-    AND exam_id = :exam_id
-        )");
-
-    $course_id = intval($_POST["course"]);
-    $exam_id = getExamId($_POST["exam"], $course_id);
-
-    $statement -> bindParam(":exam_id", $exam_id);
-    $statement -> bindParam(":course_id", $course_id);
-    $statement -> execute();
-    $dbh -> commit();
-    $result = $statement -> fetchAll();
-
-    $headers = array("c_id", "total", "exam_name", "Completed", "Minimum", "Maximum", "Average");
-    createTable($result, $headers);
-    $dbh = null;
-}
-
-function getSubmissions(){
-    $dbh = connectDB();
-    $dbh -> beginTransaction();
-    $statement = $dbh -> prepare("SELECT s.student_id, s.name, start_time, end_time, score FROM takes_exam te JOIN students s ON te.student_id = s.student_id JOIN exams e ON te.exam_id = e.exam_id JOIN courses c on e.course_id = c.course_id WHERE e.name = :exam AND c.course_id = :course");
-    $statement -> bindParam(":exam", $_POST["exam"]);
-    $statement -> bindParam(":course", $_POST["course"]);
-    $statement -> execute();
-    $result = $statement -> fetchAll();
-
-
-    $headers = array("id", "name", "start_time", "end_time", "score");
-    createTable($result, $headers);
-    $dbh = null;
-}
 ?>
 
 <html lang="en">
